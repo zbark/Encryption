5416cb5289504c0e78b4122ba6d9d1f263e3fcaf
zbark <38356551+zbark@users.noreply.github.com>
2018-04-13 14:22:04 -0400

    Update mainClass.java

--- Encryption/src/Encryption/mainClass.java
+++ Encryption/src/Encryption/mainClass.java
@@ -5,11 +5,7 @@ public class mainClass
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		//hello world comments
-		//yo what is up
-		//this is a compileable branch
+
 	}
 
 }
